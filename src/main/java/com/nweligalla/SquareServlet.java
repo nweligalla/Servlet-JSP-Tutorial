592a88b2b595b6ccdad9f1dee7cf3d4c618777c8
_nayanaw <nayanaw@auradot.com>
2021-04-23 14:57:55 +0530

    session and cookies

--- src/main/java/com/nweligalla/SquareServlet.java
+++ src/main/java/com/nweligalla/SquareServlet.java
@@ -3,16 +3,37 @@ package com.nweligalla;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SquareServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
-		//get attribite from req object
-		int k =Integer.parseInt(req.getParameter("k"));
+//		//session methods
+//		HttpSession session =req.getSession();
+//		
+//		int k = (int)session.getAttribute("k");
+		
+//		//delete session data
+//		session.removeAttribute("k");
+		
+		
+		int k=0;
+		
+		
+		//accept cookie
+		Cookie cookies[] =req.getCookies();
+		
+		for(Cookie c:cookies) {
+			if(c.getName().equals("k"))
+				k=Integer.parseInt(c.getValue());
+		}
+		
+		
 		
 		k*=k;
 		
