d102094b1dbb2c0a6458680de117cd62a0dfe667
_nayanaw <nayanaw@auradot.com>
2021-04-23 16:08:43 +0530

    added annotations rather thans xml

--- src/main/java/com/nweligalla/AddServelet.java
+++ src/main/java/com/nweligalla/AddServelet.java
@@ -5,15 +5,23 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
+
+//anotations
+
+@WebServlet("/add")
 public class AddServelet extends HttpServlet{
 	
 	//for get methods use doGet()
+	
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
--- src/main/java/com/nweligalla/MyServlet.java
+++ src/main/java/com/nweligalla/MyServlet.java
@@ -6,10 +6,15 @@ import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//anotations
+
+@WebServlet("/my")
 public class MyServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
